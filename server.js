611301acf23dd405065f5a4773ff7a8378ff75aa
jschrbr <schrbrr@gmail.com>
2020-06-13 04:23:19 +1000

    https:)

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require("express");
-const logger = require("morgan");
+// const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const sslRedirect = require("heroku-ssl-redirect");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 // app.use(logger("dev"));
-
+app.use(sslRedirect());
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -24,16 +25,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 // routes
 
 app.use(require("./routes/api.js"));
-
-if (process.env.NODE_ENV === "production") {
-  app.use((req, res, next) => {
-    if (req.secure) {
-      next();
-      return;
-    }
-    res.redirect(`https://${req.headers.host + req.url}`);
-  });
-}
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
